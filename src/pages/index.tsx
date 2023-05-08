958b8d27c9630c330fa769bb992a0d458f8f4d21
JosephO1992 <josephraoreilly@gmail.com>
2023-05-08 11:00:01 +0100

    refactor: using <Suspense /> component and added extra error handling.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 // import React, { useState, useEffect } from 'react'
-import { useDeferredValue, useEffect, useState } from 'react';
-import { useLocationSearch } from '@/lib/hook';
+import { useDeferredValue, useEffect, useState, Suspense } from 'react';
 import Loader from '@/components/Loader';
 import ErrorMessage from '@/components/ErrorMessage';
 import Forecast from '@/components/Forecast';
@@ -21,29 +20,46 @@ const deferredLocation = useDeferredValue(location)
 const [forecastData, setForecastData] = useState(null)
 const [isLoading, setIsLoading] = useState(false)
 const [isError, setIsError] = useState(false)
+const [errorMessage, setErrorMessage] = useState('')
 
 async function getLocation() {
   let locationToSearch = location;
   setIsError(false)
+  setErrorMessage('')
 
   if(!location) {
     locationToSearch = 'London'
   }
-  const locationArray = await fetch(`${process.env.NEXT_PUBLIC_API_URI_LOCATIONS}cities/search?apikey=${process.env.NEXT_PUBLIC_API_KEY}&q=${locationToSearch}`, {
-    'method': 'GET',
-  }).then(response => response.json())
-
-  if(!locationArray.length) {
-    setIsLoading(false)
-    setIsError('There were no results returned from your query, try something else')
-    return
+  try {
+    const locationArray = await fetch(`${process.env.NEXT_PUBLIC_API_URI_LOCATIONS}cities/search?apikey=${process.env.NEXT_PUBLIC_API_KEY}&q=${locationToSearch}`, {
+      'method': 'GET',
+    }).then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+        setIsError(true);
+        setErrorMessage('Error getting the location')
+      }
+    })
+  
+    if(!locationArray.length) {
+      setIsLoading(false)
+      setIsError(true)
+      setErrorMessage('There were no results returned from your query, try something else')
+      return
+    }
+  
+    getForecastData(locationArray[0].Key)
+    
+  } catch (error) {
+    setIsError(true);
+    setErrorMessage('Error getting the location')
   }
-
-  getForecastData(locationArray[0].Key)
+  
 }
 
 async function getForecastData(locationKey:number) {
-  const data =  await fetch(`${process.env.NEXT_PUBLIC_API_URI_FORECAST}/daily/1day/${locationKey}?apikey=${process.env.NEXT_PUBLIC_API_KEY}`).then(response => response.json());
+  const data =  await fetch(`${process.env.NEXT_PUBLIC_API_URI_FORECAST}daily/1day/${locationKey}?apikey=${process.env.NEXT_PUBLIC_API_KEY}`).then(response => response.json());
   
 
   setForecastData(data)
@@ -71,13 +87,16 @@ useEffect(() => {
         <h1 className='text-3xl text-white font-bold uppercase text-center h-20 mb-8'>Weather.app</h1>
         <input type="text" className='bg-white rounded-full h-12 px-4 focus-visible:ring-0 ring-0' value={location} onChange={e => setLocation(e.target.value)}/>
       </div>
-      <div>
+      {/* <div>
         {isLoading ? <Loader /> 
-          : isError ? <ErrorMessage message={isError}/> 
+          : isError ? <ErrorMessage message={errorMessage}/> 
           : forecastData ? <Forecast data={forecastData}/> 
           : 'something'}
        
-      </div>
+      </div> */}
+      <Suspense fallback={<h2>Loading...</h2>}>
+        {isError ? <ErrorMessage message={errorMessage}/> : forecastData ? <Forecast data={forecastData} location={deferredLocation}/> : 'something'}
+      </Suspense>
     </main>
   )
 }
