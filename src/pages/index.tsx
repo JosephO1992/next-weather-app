65fd813827fb340cf6b588b41f6f469843cb4a31
JosephO1992 <josephraoreilly@gmail.com>
2023-05-08 09:00:58 +0100

    refactor: changed to client side fetching in index.tsx, get data back and displaying

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Inter } from 'next/font/google'
 // import React, { useState, useEffect } from 'react'
-import { useLondon } from '@/lib/hook';
+import { useEffect, useState } from 'react';
+import { useLocationSearch } from '@/lib/hook';
 import Loader from '@/components/Loader';
 import ErrorMessage from '@/components/ErrorMessage';
 import Forecast from '@/components/Forecast';
 import { dummyData } from '../../dummyData';
+import { fetcher } from '@/lib/fetcher';
 
 
 import useSWR from 'swr';
@@ -14,23 +16,54 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-// const {data, isLoading, isError } = useLondon()
+const [location, setLocation] = useState('London');
+const [forecastData, setForecastData] = useState(null)
+const [isLoading, setIsLoading] = useState(false)
+const [isError, setIsError] = useState(false)
 
-const data = dummyData
-const isError = false
-const isLoading = false
+async function getLocation() {
+  const locationArray = await fetch(`${process.env.NEXT_PUBLIC_API_URI_LOCATIONS}cities/search?apikey=${process.env.NEXT_PUBLIC_API_KEY}&q=${location}`, {
+    'method': 'GET',
+  }).then(response => response.json())
 
-console.log(data);
+  getForecastData(locationArray[0].Key)
+}
+
+async function getForecastData(locationKey:number) {
+  const data =  await fetch(`${process.env.NEXT_PUBLIC_API_URI_FORECAST}/daily/1day/${locationKey}?apikey=${process.env.NEXT_PUBLIC_API_KEY}`).then(response => response.json());
+  
+
+  setForecastData(data)
+  setIsLoading(false)
+}
+
+useEffect(() => {
+
+  setIsLoading(true)
+
+  getLocation()
+}, [])
+
+
+
+// const data = dummyData
+// const isError = false
+// const isLoading = false
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 bg-sky-500 ${inter.className}`}
     >
-      <div className='w-1/3 flex flex-col justify-center'>
+      <div className='w-1/3 flex flex-col justify-center mb-8'>
         <h1 className='text-3xl text-white font-bold uppercase text-center h-20 mb-8'>Weather.app</h1>
         <input type="text" className='bg-white rounded-full h-12 px-4 focus-visible:ring-0 ring-0'/>
       </div>
       <div>
-        {isLoading ? <Loader /> : isError ? <ErrorMessage /> : <Forecast data={data}/>}
+        {isLoading ? <Loader /> 
+          : isError ? <ErrorMessage /> 
+          : forecastData ? <Forecast data={forecastData}/> 
+          : 'something'}
+       
       </div>
     </main>
   )
